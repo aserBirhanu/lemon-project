6e88cc0e61592d31997a079d7a26701f2e4b231f
aserBirhanu <155755382+aserBirhanu@users.noreply.github.com>
2024-04-17 09:50:33 +0300

    little-lemon menu project

---
+++ little-lemon-Menu/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Manu from './components/Menu';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+
+    <>
+    <Nav/>
+     <Main/>
+     <Manu/>
+     <Footer/>
+    </>
+  );
+}
+
+export default App;
